6b559c19b9b14307294ec1d21213961805059b34
Giedrius Rimkus <giedrius.rimkus.a@gmail.com>
2014-10-30 14:09:37 +0200

    Added devise and users

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+gem 'devise', '~> 3.3.0'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Blog::Application.routes.draw do
+  devise_for :users
   resources :posts do
   	resources :comments
   end
   root "posts#index"
 
   get '/about', to: 'pages#about'
+  match '/admin',  to: 'devise/sessions#new', via: 'get'
+
 end
